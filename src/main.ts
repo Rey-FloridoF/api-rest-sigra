2a4aa03c5633cd14d93050de6e29f11193336344
Rey-FloridoF <floridofer0306@gmail.com>
2026-03-19 14:50:30 -0400

    servidor real conectado

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'http://localhost:3000'
+      'https://fe-sigra.vercel.app'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
